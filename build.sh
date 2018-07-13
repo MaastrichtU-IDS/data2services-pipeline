b2caa0fd56df5b23412f668e9c786ec9a4eb2dea
Alexander Malic <alexander.malic@gmail.com>
2018-07-13 15:39:40 +0200

    added graphdb

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ docker build -t apache-drill ./apache-drill
 docker build -t autodrill ./AutoDrill
 docker build -t r2rml ./r2rml
 docker build -t rdf-upload ./RdfUpload
+docker build -t graphdb ./graphdb
