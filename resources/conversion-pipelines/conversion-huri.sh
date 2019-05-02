db205b5165fdb9f83f2c7e76de7eecf57b448a47
vemonet <vincent.emonet@gmail.com>
2019-05-02 10:37:31 +0200

    fix conversion scripts

--- resources/conversion-pipelines/conversion-huri.sh
+++ resources/conversion-pipelines/conversion-huri.sh
@@ -16,8 +16,8 @@ docker run -it --rm --link drill:drill \
 
 # Load RDF file in GraphDB ncats-test repository
 
-docker run -d --name convert_huri --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/huri" -ep "http://graphdb:7200/repositories/ncats-red-kg/statements" -un emonet -pw jenaipasfini34 -var serviceUrl:http://localhost:7200/repositories/ncats-test inputGraph:http://data2services/graph/autor2rml/huri outputGraph:https://w3id.org/data2services/graph/biolink/huri
+docker run -d --name convert_huri --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/huri" -ep "http://graphdb:7200/repositories/ncats-red-kg/statements" -un emonet -pw $PASSWORD -var serviceUrl:http://localhost:7200/repositories/ncats-test inputGraph:http://data2services/graph/autor2rml/huri outputGraph:https://w3id.org/data2services/graph/biolink/huri
 
-docker run -d --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/compute-hcls-stats" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw jenaipasfini34 -var inputGraph:https://w3id.org/data2services/graph/biolink/huri
+docker run -d --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/compute-hcls-stats" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw $PASSWORD -var inputGraph:https://w3id.org/data2services/graph/biolink/huri
 
-#docker run -d --link graphdb:graphdb -v "/home/emonet/data2services-insert/compute-hcls-stats":/data data2services-sparql-operations -f "/data" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw jenaipasfini34 -var inputGraph:https://w3id.org/data2services/graph/biolink/huri
+#docker run -d --link graphdb:graphdb -v "/home/emonet/data2services-insert/compute-hcls-stats":/data data2services-sparql-operations -f "/data" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw $PASSWORD -var inputGraph:https://w3id.org/data2services/graph/biolink/huri
--- resources/conversion-pipelines/conversion-uniprot.sh
+++ resources/conversion-pipelines/conversion-uniprot.sh
@@ -1,10 +1,10 @@
                                                      
 # UniProt from https://sparql.uniprot.org/
 
-docker run -it --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/uniprot" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw jenaipasfini34 -var outputGraph:https://w3id.org/data2services/graph/biolink/uniprot
+docker run -it --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/uniprot" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw $PASSWORD -var outputGraph:https://w3id.org/data2services/graph/biolink/uniprot
 
 # Only human
-docker run -it --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/uniprot" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw jenaipasfini34 -var outputGraph:https://w3id.org/data2services/graph/biolink/uniprot/human
+docker run -it --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/uniprot" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw $PASSWORD -var outputGraph:https://w3id.org/data2services/graph/biolink/uniprot/human
 
 
-docker run -d --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/compute-hcls-stats" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw jenaipasfini34 -var inputGraph:https://w3id.org/data2services/graph/biolink/uniprot
+docker run -d --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/compute-hcls-stats" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw $PASSWORD -var inputGraph:https://w3id.org/data2services/graph/biolink/uniprot
