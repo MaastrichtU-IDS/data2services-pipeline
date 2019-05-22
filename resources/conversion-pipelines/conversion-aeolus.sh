070e482af3c5f114bbf1b9c2194c7a2dfe582bc4
vemonet <vincent.emonet@gmail.com>
2019-05-22 10:44:11 +0200

    add detail to aeolus and cohd pipelines and create reactome pipeline

--- resources/conversion-pipelines/conversion-aeolus.sh
+++ resources/conversion-pipelines/conversion-aeolus.sh
@@ -5,12 +5,16 @@ docker run -it --rm -v /data/emonet/ncats/aeolus:/data data2services-download --
 
 docker run -it --rm -v /data/data2services:/data data2services-download --download-datasets aeolus
 
-docker run -it -v /data/data2services/aeolus:/data json2xml -i /data/drugpairReactionCounts_1.json  -o /data/drugpairReactionCounts_1.xml
+docker run -it -v /data/data2services/aeolus:/data json2xml -i /data
+
+docker run -it -v /data/data2services/aeolus:/data xml2rdf -i /data  -o /data/output_aeolus.nq -g https://w3id.org/data2services/graph/xml2rdf/aeolus 
 
 docker run -it -v /data/data2services/aeolus:/data xml2rdf -i /data/drugpairReactionCounts_1.xml  -o /data/drugpairReactionCounts_1.nq -g https://w3id.org/data2services/graph/xml2rdf/aeolus 
 # -n http://ids.unimaas.nl/xml2rdf/data/
 
+for file in /data/data2services/aeolus/*.xml; do docker run -it -v /data:/data xml2rdf -i $file -o $file.nq -g https://w3id.org/data2services/graph/xml2rdf/aeolus ; done 
 
+scp *.nq ids2:/home/emonet/aeolus
 
 # Load RDF file in GraphDB ncats-test repository
 
--- resources/conversion-pipelines/conversion-cohd.sh
+++ resources/conversion-pipelines/conversion-cohd.sh
@@ -5,6 +5,8 @@ docker run --rm --name cohd-mariadb -v /data:/data -e MYSQL_ROOT_PASSWORD=pwd -d
 
 # Connect to MySQL
 docker exec -it cohd-mariadb mysql -uroot -ppwd
+\r cohd
+
 
 # AutoR2RML
 docker run -it --rm --link cohd-mariadb:cohd-mariadb -v /data/emonet/ncats/cohd/convert:/data autor2rml \
@@ -18,8 +20,22 @@ docker run -it --rm --link cohd-mariadb:cohd-mariadb -v /data/emonet/ncats/cohd/
 docker run -d --rm --link cohd-mariadb:cohd-mariadb \
   -v /data/emonet/ncats/cohd/convert:/data \
   r2rml /data/config.properties
-# sharp_ramanujan
+# OutOfMemory Error: 2e2ef5cbd80323e6a2c7d3ffc334d45f9b0e40718701624bb344e6bef262d89b
+# Without the big mapping: 4a509a1869a9fbd1721d6adb589aaffcafa74383ae7277895af2567158cfee72
+# Run the big: 8ebc1320eadd4883c779c166320107d093e99614f2c50df5f28353da2f239a9b
 
+# COHD tables size in MB:
+# cohd               | concept_pair_counts                                |    1937.98 |
+# cohd               | concept                                            |       7.55 |
+# cohd               | concept_counts                                     |       1.88 |
+select row_number() over () as autor2rml_rownum
+         , concept_id_1
+         , concept_id_2
+         , concept_count
+         , concept_frequency
+       from concept_pair_counts;
+# Without row number: 32788901 rows in set (54.859 sec)
+#With row: 32788901 rows in set (2 min 15.783 sec)
 
 # Load RDF file in GraphDB ncats-test repository
 docker run -d --name convert_cohd --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-insert/tree/master/insert-biolink/cohd" -ep "http://graphdb:7200/repositories/ncats-red-kg/statements" -un emonet -pw $PASSWORD -var serviceUrl:http://localhost:7200/repositories/ncats-test inputGraph:https://w3id.org/data2services/graph/autor2rml/cohd outputGraph:https://w3id.org/data2services/graph/biolink/cohd
